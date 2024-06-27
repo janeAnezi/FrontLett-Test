faa9df27e5d3fd176aace5bee7197c724b21c51c
Anezijane <ahnnieanezi@gmail.com>
2024-06-27 23:48:43 +0100

    update footer content

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,7 +6,7 @@ import footersc2 from "../assets/images/footersc2.png"
 function Footer() {
   return (
     <div className="bg-slate-800 text-white pb-20">
-        <div className="mx-20">
+        <div className="mx-10">
           <h3 className="font-bold text-4xl pb-3 pt-10">Newsletter</h3>
           <p className="text-sm ">Get the latest about SINC Partners, our startup Incubator program, Portfolio company offerings - straight into your inbox.</p>
 
@@ -17,9 +17,9 @@ function Footer() {
           <div className="flex mt-10 py-4 border-t border-b">
             <FooterCard title="">
               <p>
-                <span className="text-3xl tracking-widest border">SINC</span><span className=" block">PARTNERS</span> 
+                <span className="text-4xl tracking-widest border">SINC</span><span className=" block">PARTNERS</span> 
               </p>
-              <p className="py-4 text-sm">SINC Partners is a service incubation company connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%)</p> 
+              <p className="py-4 text-sm w-32">SINC Partners is a service incubation company connecting experts in product development and growth marketing willing to offer their services to amazing startups in exchange for minute equity (usually 0.5% to 2%)</p> 
             </FooterCard>
             <FooterCard title="">
               <p className="font-semibold">Platforms</p>
@@ -107,23 +107,33 @@ function Footer() {
                   <li>Kofoundme</li>
                   <li>Kofoundme</li>
                 </ul> 
-                <div className="flex">
-                  <img src={footersc} alt="footer sticker" />
-                  <p></p>
-                  <img className="w-6 h-6 mt-3 ml-2" src={whatsapp} alt="whatsapp icon" />
-                  <p></p>
+                <div className="flex mt-20">
+                  <div>
+                    <img src={footersc} alt="footer sticker" />
+                    <p className="text-sm">Trusted Buisness</p>
+                  </div>
+                  <div>
+                    <img className="w-9 h-9 mt-8 ml-5" src={whatsapp} alt="whatsapp icon" />
+                    <p className="text-sm">Chat with us</p>
+                  </div>
+                  
                 </div>
                 
               </FooterCard>
             </div>
             
           </div>
-          <div className="flex">
-            <p className="text-sm">Guaranteed 2x on your service or cash investment, usually been the first to invest. Get in early and SINC your guaranty!</p>
+          <div className="flex justify-between mt-6">
+            <p className="text-sm mt-4">Guaranteed 2x on your service or cash investment, usually been the first to invest. Get in early and SINC your guaranty!</p>
             <div className="flex">
-              <img src={footersc2} alt="company logo" />
-              <p>We are a business built on the <br /> foundation of Christian values and belief</p>
+              <img className="w-24 h-16 mr-3" src={footersc2} alt="company logo" />
+              <p className="text-sm mt-1">We are a business built on the <br /> foundation of Christian values and belief</p>
             </div>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
         </div>
         
