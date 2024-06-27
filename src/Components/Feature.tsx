3490e43b335bce01ed7fdb5501f162a1fc6e079e
Anezijane <ahnnieanezi@gmail.com>
2024-06-27 21:32:21 +0100

    add card content

--- src/Components/Feature.tsx
+++ src/Components/Feature.tsx
@@ -1,22 +1,46 @@
-import ft1 from "../assets/images/ft1.jpg"
-import ftlogo from "../assets/images/ft-lgo.jpg"
+import ft1 from "../assets/images/ft1.jpg";
+import ft2 from "../assets/images/ft2.jpg";
+import ftlogo from "../assets/images/ft-lgo.jpg";
 
 function Feature() {
+    const cards = [
+        {
+          image: ft1,
+          title: "SINC Partners invests over 200 million naira in 5 startups",
+          description: "SINC Partners invests over 200 million naira in 5 startups. SINC Partners invests over 200 million naira in 5 startups. SINC Partners invests over 200 million naira in ...",
+          reporter: "Rema Viel"
+        },
+        {
+          image: ft2,
+          title: "Another investment story",
+          description: "Another investment story with different details. Another investment story with different details. Another investment story with different details ...",
+          reporter: "Jane Doe"
+        },
+        {
+          image: ft1,
+          title: "SINC Partners invests over 200 million naira in 5 startups",
+          description: "SINC Partners invests over 200 million naira in 5 startups. SINC Partners invests over 200 million naira in 5 startups. SINC Partners invests over 200 million naira in ...",
+          reporter: "Rema Viel"
+        }
+      ];
   return (
     <div className="bg-slate-50 text-center pb-20">
-        <h3 className="font-bold text-xl pb-10">As Featured In</h3>
+      <h3 className="font-bold text-xl pb-10">As Featured In</h3>
 
-        <div className="flex overflow-x-auto space-x-2 hide-scrollbar">
-            <div className="flex">
-                <img src={ft1} alt="feature image" />
-                <div className="text-left px-10 pt-5">
-                    <img className="mb-3" src={ftlogo} alt="feature logo" />
-                    <p className="font-semibold text-lg w-64 mb-3">SINC Partners invests  over 200 million naira in 5 startups</p>
-                    <p className="w-60 mb-10">SINC Partners invests  over 200 million naira in 5 startups. SINC Partners invests  over 200 million naira in 5 startups SINC Partners invests  over 200 million naira in ...</p>
-                    <p>REPORTED BY: <br /> Rema Viel</p>
-                </div>
+      <div className="flex overflow-x-auto space-x-4 hide-scrollbar px-4">
+      {cards.map((card, index) => (
+          <div key={index} className="flex flex-shrink-0 bg-white rounded-lg shadow-lg overflow-hidden">
+            <img src={card.image} alt="feature image" className="w-2/3 object-cover" />
+            <div className="text-left px-6 py-4 w-1/3">
+              <img className="mb-3" src={ftlogo} alt="feature logo" />
+              <p className="w-60 font-semibold text-lg mb-3">{card.title}</p>
+              <p className="w-60 mb-10">{card.description}</p>
+              <p>REPORTED BY: <br /> <br /> {card.reporter}</p>
             </div>
-        </div>
+          </div>
+        ))}
+      </div>
+
       <style>{`
         .hide-scrollbar::-webkit-scrollbar {
           display: none;
@@ -27,7 +51,7 @@ function Feature() {
         }
       `}</style>
     </div>
-  )
+  );
 }
 
-export default Feature
+export default Feature;
